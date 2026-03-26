691d79a4094cdd53889ca210b779bd1de70777ea
Azat Satklycov <azat.satklycov@broadcom.com>
2026-03-26 13:55:02 +0100

    clean-up

--- src-js-deep/good-parts/continue.js
+++
@@ -1,6 +0,0 @@
-
-/**
- * continue Statement
-The continue statement jumps to the top of the loop. I have never seen a piece of
-code that was not improved by refactoring it to remove the continue statement.
- */
--- src-js-deep/t-temporal.js
+++
@@ -1,3 +0,0 @@
-//see  https://www.w3schools.com/js/js_temporal_intro.asp
-//very new API, influenced from JAVA 
-
